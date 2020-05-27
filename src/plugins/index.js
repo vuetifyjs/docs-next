3e23a832159e7ab81e6f314592da79ff4a633456
Albert Kaaman <albert@kaaman.nu>
2020-05-27 11:27:38 +0200

    refactor: make eventual transition to vue 3 easier

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,6 +4,14 @@
  * Automatically included in `./src/main.js`
  */
 
-import './app'
-import './webfontloader'
-import './vue-meta'
+import { registerComponents } from './app'
+import { loadFonts } from './webfontloader'
+import { useMeta } from './vue-meta'
+import { useVuetify } from './vuetify'
+
+export function registerPlugins (app) {
+  registerComponents(app)
+  loadFonts(app)
+  useMeta(app)
+  useVuetify(app)
+}
