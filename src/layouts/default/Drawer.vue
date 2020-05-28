e6a868eade5b1590b317c37ee532b85157600caa
John Leider <john.j.leider@gmail.com>
2020-05-28 15:44:10 -0500

    refactor(app): reduce code needed to generate items, add trailing slashes

--- src/layouts/default/Drawer.vue
+++ src/layouts/default/Drawer.vue
@@ -4,31 +4,50 @@
       dense
       nav
     >
-      <v-list-group
-        v-for="item in items"
-        :key="item.title"
-        :group="item.group"
-        :prepend-icon="item.icon"
-        no-action
-      >
-        <template v-slot:activator>
+      <template v-for="(item, i) in items">
+        <v-list-group
+          v-if="item.items"
+          :key="item.title"
+          :group="item.group"
+          :prepend-icon="item.icon"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </template>
+
+          <template v-if="item.items">
+            <v-list-item
+              v-for="subItem in item.items"
+              :key="subItem.title"
+              :to="subItem.to"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="subItem.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list-group>
+
+        <v-list-item
+          v-else
+          :key="i"
+          :to="undefined"
+          :href="item.href"
+          :target="item.href ? '_blank' : undefined"
+          :rel="item.href ? 'nofollow' : undefined"
+        >
+          <v-list-item-icon v-if="item.icon">
+            <v-icon v-text="item.icon" />
+          </v-list-item-icon>
+
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
-        </template>
-
-        <template v-if="!!item.items">
-          <v-list-item
-            v-for="subItem in item.items"
-            :key="subItem.title"
-            :to="subItem.to"
-          >
-            <v-list-item-content>
-              <v-list-item-title v-text="subItem.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list-group>
+        </v-list-item>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,47 +1,27 @@
 // Pathify
 import { make } from 'vuex-pathify'
 
+// Plugins
 import { i18n } from '@/plugins/i18n'
+
+// Utilities
+import { kebabCase } from 'lodash'
+import { ROOT_DISPATCH } from '@/store'
+
+// Data
 import nav from '@/data/nav'
 
-const state = {
-  nav: [],
-}
+const state = {}
 
 const mutations = make.mutations(state)
 
 const actions = {
   init: ({ dispatch, rootState }) => {
-    dispatch('ads/fetch', null, { root: true })
-    dispatch('i18n/switch', { locale: rootState.route.params.locale }, { root: true })
+    const { locale } = rootState.route.params
+
+    dispatch('ads/fetch', null, ROOT_DISPATCH)
+    dispatch('i18n/switch', { locale }, ROOT_DISPATCH)
   },
-}
-
-function findItems (group, rootState) {
-  const foundItems = Object.keys(rootState.i18n.pages).filter(p => p.startsWith(group) && p !== group).map(p => {
-    return {
-      title: rootState.i18n.pages[p],
-      to: `/${rootState.route.params.locale}${p}`,
-    }
-  })
-
-  return foundItems.length ? foundItems : undefined
-}
-
-function genItem (item, parent, rootState) {
-  const group = `${parent}/${item.title}`
-
-  return {
-    // Try finding title among imported pages, otherwise try translating it
-    title: rootState.i18n.pages[group] || i18n.t(item.title),
-    icon: item.icon || undefined,
-    group,
-    // If there are no defined components, try finding them among imported pages
-    items: item.items
-      ? item.items.map(i => genItem(i, group, rootState))
-      : findItems(group, rootState),
-    to: `/${rootState.route.params.locale}${group}`,
-  }
 }
 
 const getters = {
@@ -56,4 +36,42 @@ export default {
   mutations,
   actions,
   getters,
+}
+
+function genItems (item, ...args) {
+  return item.items
+    ? item.items.map(i => genItem(i, ...args))
+    : findItems(...args)
+}
+
+function findItems (group, { i18n, route }) {
+  const pages = []
+  const { locale } = route.params
+
+  for (const key in i18n.pages) {
+    if (!key.startsWith(group) || group === key) continue
+
+    pages.push({
+      title: i18n.pages[key],
+      to: `/${locale}${key}/`,
+    })
+  }
+
+  return pages.length > 0 ? pages : undefined
+}
+
+function genItem (item, parent, rootState) {
+  const path = kebabCase(item.to || item.title)
+  const group = `${parent}/${path}`
+  const pages = rootState.i18n.pages
+  const title = pages[group] || item.title
+
+  return {
+    group,
+    href: item.href || undefined,
+    icon: item.icon || undefined,
+    items: genItems(item, group, rootState),
+    title: i18n.te(title) ? i18n.t(title) : title,
+    to: !item.href ? `${path}/` : '',
+  }
 }
