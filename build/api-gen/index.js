2a0cbff52ac5abe87ad424609473f74554c341e2
John Leider <john.j.leider@gmail.com>
2020-06-04 12:24:59 -0500

    style(api-gen): update code formatting

--- build/api-gen/index.js
+++ build/api-gen/index.js
@@ -1,24 +1,32 @@
+// Imports
 const { readFileSync } = require('fs')
 const { resolve } = require('path')
 
 // pseudo code to mock api-gen
 function generateAPI (comp, locale) {
+  const api = {}
   const compMap = require(`./maps/${comp}`)[comp]
   const localePath = resolve(__dirname, `./locale/${locale}/${comp}.json`)
   const compLocale = JSON.parse(readFileSync(localePath, 'utf8'))
-  const api = {}
+
   for (const [section, sValue] of Object.entries(compMap)) {
     const items = []
+
     if (section === 'mixins') {
       api.mixins = sValue
+
       continue
     }
+
     for (const item of sValue) {
       const description = compLocale[section][item.name] || ''
+
       items.push(Object.assign(item, { description }))
     }
+
     api[section] = items
   }
+
   return api
 }
 
