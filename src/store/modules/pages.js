fa5f21b6e0f0912f5117c4516a04ef098ca5f4d1
John Leider <john.j.leider@gmail.com>
2020-06-20 12:43:56 -0500

    docs(store): rename i18n to pages
    moved toc from page to new pages module and removed page as well

--- src/store/modules/pages.js
+++ src/store/modules/pages.js
@@ -1,8 +1,12 @@
 // Pathify
 import { make } from 'vuex-pathify'
 
-// Data
+// Language
+// import { loadLocale } from '@/plugins/i18n'
+
 const state = {
+  frontmatter: {},
+  pages: {},
   toc: [],
 }
 
@@ -10,7 +14,11 @@ const mutations = make.mutations(state)
 
 const actions = {}
 
-const getters = {}
+const getters = {
+  translating: (_, __, rootState) => {
+    return rootState.route.params.locale === 'eo-UY'
+  },
+}
 
 export default {
   namespaced: true,
