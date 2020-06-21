8295fcff65369a580badd70ccffe73a8c5a69da8
John Leider <john.j.leider@gmail.com>
2020-06-21 13:38:18 -0500

    performance(DocumentationDrawer): add keep-alive process for nav

--- src/layouts/documentation/Drawer.vue
+++ src/layouts/documentation/Drawer.vue
@@ -5,64 +5,19 @@
     clipped
     width="300"
   >
-    <v-list
-      dense
-      expand
-      nav
-    >
-      <template v-for="(item, i) in items">
-        <v-list-group
-          v-if="item.items"
-          :key="`${item.title}-${i}`"
-          :group="item.to"
-          :prepend-icon="item.icon"
-          no-action
-        >
-          <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title
-                v-if="item.title"
-                v-text="item.title"
-              />
-            </v-list-item-content>
-          </template>
+    <keep-alive>
+      <documentation-list
+        v-if="advanced"
+        key="advanced"
+        :items="anav"
+      />
 
-          <template v-if="item.items">
-            <v-list-item
-              v-for="(subItem, j) in item.items"
-              :key="`${subItem.title}-${j}`"
-              :to="subItem.to"
-            >
-              <v-list-item-icon v-if="subItem.icon">
-                <v-icon v-text="subItem.icon" />
-              </v-list-item-icon>
-
-              <v-list-item-content>
-                <v-list-item-title v-text="subItem.title" />
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list-group>
-
-        <v-list-item
-          v-else
-          :key="i"
-          :href="item.href"
-          :rel="item.href ? 'nofollow' : undefined"
-          :target="item.href ? '_blank' : undefined"
-          :to="item.to"
-          color="primary"
-        >
-          <v-list-item-icon v-if="item.icon">
-            <v-icon v-text="item.icon" />
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
+      <documentation-list
+        v-else
+        key="simple"
+        :items="nav"
+      />
+    </keep-alive>
   </v-navigation-drawer>
 </template>
 
@@ -73,14 +28,13 @@
   export default {
     name: 'DocumentationDrawer',
 
+    components: { DocumentationList: () => import('./List') },
+
     computed: {
-      advanced: sync('user/drawer@advanced'),
       drawer: sync('app/drawer'),
+      advanced: get('user/drawer@advanced'),
       anav: get('app/advanced'),
       nav: get('app/nav'),
-      items () {
-        return this.advanced ? this.anav : this.nav
-      },
     },
   }
 </script>
