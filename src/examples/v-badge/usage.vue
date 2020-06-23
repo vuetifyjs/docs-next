1ec3e0dfcdba9caa1c3fa191c2fc6c2e92f95774
Kester Lotz <kester.lotz@posteo.de>
2020-06-23 22:37:39 +0200

    docs(v-badge): add examples and usage, transferred content from .pug and edit API language data

---
+++ src/examples/v-badge/usage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    style="width: 100%"
+    class="text-center"
+  >
+    <v-badge
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <v-icon large>
+        $mdiVuetify
+      </v-icon>
+    </v-badge>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      defaults: {
+        dot: false,
+        overlap: false,
+        icon: false,
+        left: false,
+        bottom: false,
+      },
+      options: {
+        booleans: ['dot', 'overlap', 'icon', 'left', 'bottom'],
+      },
+      tabs: ['default', 'hidden', 'text', 'inline', 'borderd'],
+    }),
+  }
+</script>
