7e8a0c20237a6c1d50f8260a23d0b2c0383ba99e
Kester Lotz <kester.lotz@posteo.de>
2020-06-24 18:40:18 +0200

    docs(v-banner): convert page, transfer and convert usage, add API, add examples, add api lang data

---
+++ src/examples/v-banner/usage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="width: 100%;">
+    <v-banner
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      One line message text string with two actions on tablet / Desktop
+      <template
+        v-if="$attrs.action"
+        v-slot:actions
+      >
+        <v-btn
+          text
+          color="deep-purple accent-4"
+        >
+          Action
+        </v-btn>
+      </template>
+    </v-banner>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      defaults: {
+        action: false,
+        icon: null,
+        'single-line': null,
+        sticky: null,
+      },
+      options: {
+        booleans: ['action'],
+        selects: {
+          icon: ['$mdiAccount', '$mdiVuetify'],
+        },
+        sliders: {
+          elevation: [0, 24],
+        },
+      },
+      tabs: ['single-line', 'sticky'],
+    }),
+  }
+</script>
