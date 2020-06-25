a734f4bd9f938c7d03aebcb45b1f3c7bf2eb83ce
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-06-25 12:28:24 +0300

    docs(VImg): migrate the page

---
+++ src/examples/v-img/usage.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-img
+        src="https://picsum.photos/id/11/500/300"
+        lazy-src="https://picsum.photos/id/11/10/6"
+        aspect-ratio="1"
+        class="grey lighten-2"
+        v-bind="$attrs"
+      ></v-img>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      show: true,
+      defaults: {
+        contain: false,
+        'max-width': 250,
+        'max-height': 150,
+      },
+      options: {
+        booleans: [
+          'contain',
+        ],
+        sliders: {
+          'max-width': [0, 500],
+          'max-height': [0, 300],
+        },
+      },
+      tabs: [],
+    }),
+  }
+</script>
