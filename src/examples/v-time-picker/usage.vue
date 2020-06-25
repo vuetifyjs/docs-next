fbcd81a7bde2738a688429bd485f8b9fb084d280
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-06-25 13:32:36 +0300

    docs(VTimePicker): migrate the page

---
+++ src/examples/v-time-picker/usage.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row align="center">
+    <v-time-picker
+      v-model="picker"
+      class="mt-2"
+      v-bind="$attrs"
+    ></v-time-picker>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      picker: null,
+      defaults: {
+        disabled: false,
+        flat: false,
+        readonly: false,
+        landscape: false,
+        ampmInTitle: false,
+        useSeconds: false,
+        format: 'ampm',
+        fullWidth: false,
+        noTitle: false,
+        scrollable: false,
+      },
+      options: {
+        booleans: [
+          'disabled',
+          'flat',
+          'readonly',
+          'landscape',
+          'ampmInTitle',
+          'useSeconds',
+          'fullWidth',
+          'noTitle',
+          'scrollable',
+        ],
+        btnToggles: {
+          format: ['ampm', '24hr'],
+        },
+      },
+      tabs: [],
+    }),
+  }
+</script>
