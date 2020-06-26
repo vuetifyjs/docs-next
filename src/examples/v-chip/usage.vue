6c297c5e2e1b66c6ed1ac790093b7eaabe6c46af
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-06-26 11:14:50 +0300

    docs(VChip): migrate the page

---
+++ src/examples/v-chip/usage.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-chip
+        v-bind="$attrs"
+      >
+        Chip Component
+      </v-chip>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      show: true,
+      defaults: {
+        'close-icon': 'mdi-close',
+        color: 'blue',
+        close: false,
+        value: false,
+        filter: false,
+        label: false,
+        link: false,
+        outlined: false,
+        pill: false,
+      },
+      options: {
+        selects: {
+          'close-icon': [
+            'mdi-close-outline',
+            'mdi-close',
+            'mdi-delete',
+          ],
+          color: [
+            'red',
+            'orange',
+            'yellow',
+            'green',
+            'blue',
+            'purple',
+          ],
+        },
+        booleans: [
+          'close',
+          'value',
+        ],
+      },
+      tabs: [
+        'filter',
+        'label',
+        'link',
+        'outlined',
+        'pill',
+      ],
+    }),
+  }
+</script>
