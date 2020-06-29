080b500cd2e796fddd63d8815c6421724c384e28
unknown <austin.akers5@gmail.com>
2020-06-28 23:55:55 -0500

    finished Functional Components Section an v-card api props

--- src/examples/v-card/usage.vue
+++ src/examples/v-card/usage.vue
@@ -1,15 +1,20 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Card title
-    </v-card-title>
-    <v-card-subtitle>
-      Secondary text
-    </v-card-subtitle>
-    <v-card-text>
-      Greyhound divisively hello coldly wonderfully marginally far upon excluding.
-    </v-card-text>
-  </v-card>
+  <v-container>
+    <v-row>
+      <div class="mx-3"></div>
+      <v-card>
+        <v-card-title>
+          Card title
+        </v-card-title>
+        <v-card-subtitle>
+          Secondary text
+        </v-card-subtitle>
+        <v-card-text>
+          Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
