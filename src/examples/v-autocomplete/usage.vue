492477186049f1d4cb38436ac2bd41ad768276fa
Kester Lotz <kester.lotz@posteo.de>
2020-06-30 23:02:12 +0200

    docs(v-autocomplete): docs updated
    Migrated docs. Couldn't workout how to migrate the usage. Had problems with migrating examples,
    since some didn't fit in one of the four categories. There are two slot examples wich both display
    general uses of slots, so I renamed them to "simple" and "advanced".

---
+++ src/examples/v-autocomplete/usage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="width: 100%;">
+    <v-autocomplete
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+    </v-autocomplete>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      defaults: {
+      },
+      options: {
+        selects: {
+          filter: ['Exact Match', 'Search Length > 2 & Loose Match'],
+        },
+      },
+      tabs: [],
+    }),
+  }
+</script>
