c84ad60f26c38dd269680c82b89ff0610a841140
Kester Lotz <kester.lotz@posteo.de>
2020-07-01 22:33:21 +0200

    docs(v-avatar): docs migrated
    Migrated docs

---
+++ src/examples/v-avatar/usage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    style="width: 100%"
+    class="text-center"
+  >
+    <v-avatar
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      VJ
+    </v-avatar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      defaults: {
+        color: 'primary',
+        image: false,
+        tile: false,
+      },
+      options: {
+        selects: {
+          color: ['primary', 'accent', 'warning lighten-2', 'teal', 'grey lighten-2'],
+        },
+        booleans: ['image', 'tile'],
+        sliders: {
+          size: [56, 128],
+        },
+      },
+      tabs: [],
+    }),
+  }
+</script>
