75f5942c5d4773907db33eec53e043923daf0e1b
Kester Lotz <kester.lotz@posteo.de>
2020-07-02 23:05:14 +0200

    docs(v-bottom-sheet): docs migrated

---
+++ src/examples/v-bottom-sheet/usage.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-bottom-sheet
+        v-model="sheet"
+        v-bind="$attrs"
+        v-on="$listeners"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="purple"
+            dark
+            v-on="on"
+          >
+            Open Playground
+          </v-btn>
+        </template>
+        <v-sheet
+          class="text-center"
+          height="200px"
+        >
+          <v-btn
+            class="my-6"
+            depressed
+            color="error"
+            @click="sheet = !sheet"
+          >
+            close
+          </v-btn>
+          <div class="title">
+            Active Playground Props
+          </div>
+          <div v-if="sheet">
+            v-model
+          </div>
+          <div v-if="$attrs.inset">
+            inset
+          </div>
+          <div v-if="$attrs['hide-overlay']">
+            hide-overlay
+          </div>
+          <div v-if="$attrs.persistent">
+            persistent
+          </div>
+        </v-sheet>
+      </v-bottom-sheet>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      sheet: false,
+      defaults: {
+        'hide-overlay': false,
+        inset: false,
+        persistent: false,
+      },
+      options: {},
+      tabs: ['hide-overlay', 'inset', 'persistent'],
+    }),
+  }
+</script>
