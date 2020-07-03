cea525ca56845882ae568598258f8a4ce98d144f
Kester Lotz <kester.lotz@posteo.de>
2020-07-03 18:29:12 +0200

    docs(v-breadcrumbs): docs migrated

---
+++ src/examples/v-breadcrumbs/usage.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-breadcrumbs
+        :items="items"
+        v-bind="$attrs"
+        v-on="$listeners"
+      >
+        <template
+          v-if="!$attrs.customDivider"
+          v-slot:divider
+        >
+          <v-icon>$mdiDotsVertical</v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      items: [
+        {
+          text: 'Dashboard',
+          disabled: false,
+          href: 'breadcrumbs_dashboard',
+        },
+        {
+          text: 'Link 1',
+          disabled: false,
+          href: 'breadcrumbs_link_1',
+        },
+        {
+          text: 'Link 2',
+          disabled: true,
+          href: 'breadcrumbs_link_2',
+        },
+      ],
+      defaults: {
+        large: false,
+        customDivider: false,
+        divider: null,
+      },
+      options: {
+        booleans: ['customDivider'],
+        selects: {
+          divider: ['/', '\\', '.', ';', '>', '-'],
+        },
+      },
+      tabs: ['large'],
+    }),
+
+  }
+</script>
