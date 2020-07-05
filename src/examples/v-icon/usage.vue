e118987b540f7cbbb72399fb91cdf7ff43908b9e
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-07-05 21:21:19 +0300

    docs(VIcon): migrate the page

---
+++ src/examples/v-icon/usage.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="space-around"
+    >
+      <v-select
+        v-model="icon"
+        :items="icons"
+      ></v-select>
+      <v-icon
+        v-bind="$attrs"
+      >
+        {{ icon || 'mdi-plus' }}
+      </v-icon>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Usage',
+
+    inheritAttrs: false,
+
+    data: () => ({
+      icon: 'mdi-access-point',
+      icons: [
+        'mdi-plus',
+        'mdi-minus',
+        'mdi-access-point',
+        'mdi-antenna',
+      ],
+      defaults: {
+        dense: false,
+        color: 'green',
+        'x-small': false,
+        small: false,
+        medium: true,
+        large: false,
+        'x-large': false,
+      },
+      options: {
+        booleans: ['dense'],
+        selects: {
+          color: [
+            'red',
+            'orange',
+            'yellow',
+            'green',
+            'blue',
+            'purple',
+          ],
+        },
+      },
+      tabs: [
+        'x-small',
+        'small',
+        'medium',
+        'large',
+        'x-large',
+      ],
+    }),
+  }
+</script>
