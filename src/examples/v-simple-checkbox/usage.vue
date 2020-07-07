a3651c710c7c6323ed4c124834cac6ab981c7677
Kester Lotz <kester.lotz@posteo.de>
2020-07-07 19:25:05 +0200

    docs(simple-checkbox): split from selection control

---
+++ src/examples/v-simple-checkbox/usage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container
+    class="px-0"
+    fluid
+  >
+    <v-simple-checkbox
+      v-model="checkbox"
+    ></v-simple-checkbox>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        checkbox: true,
+      }
+    },
+  }
+</script>
