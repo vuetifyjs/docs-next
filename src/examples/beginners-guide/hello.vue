41e7d194ab3401736bbc17be80401cf19a476069
Yiping <Yiping.Ruan@outlook.com>
2020-08-22 22:17:58 +0800

    Add draft

---
+++ src/examples/beginners-guide/hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="name"
+      label="Your name"
+    ></v-text-field>
+    <div>{{ message }}</div>
+    <v-btn
+      color="success"
+      @click="greet"
+    >
+      Greet
+    </v-btn>
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      name: '',
+      message: '',
+    }),
+    methods: {
+      greet () {
+        this.message = 'Hello, ' + this.name
+      },
+    },
+  }
+</script>
