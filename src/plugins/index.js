d50c78d6ee3b5f68a89519422a3e4d52987a2759
John Leider <john.j.leider@gmail.com>
2020-08-27 13:55:11 -0500

    chore(webfontloader): re-enable library

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,16 +4,14 @@
  * Automatically included in `./src/main.js`
  */
 
-// import { loadFonts } from './webfontloader'
+import { loadFonts } from './webfontloader'
 import { registerComponents } from './app'
 // import { useMeta } from './vue-meta'
 import { useVuetify } from '@/vuetify'
-// import { IN_BROWSER } from '@/util/globals'
 
 export function registerPlugins (app) {
   registerComponents(app)
-  // FIXME: re-enable and fix SSG
-  // IN_BROWSER && loadFonts(app)
+  loadFonts(app)
   // useMeta(app)
   useVuetify(app)
 }
--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -1,28 +1,30 @@
-// FIXME: re-enable and fix SSG
-
 /**
  * plugins/webfontloader.js
  *
  * webfontloader documentation: https://github.com/typekit/webfontloader
  */
 
- // Imports
-// import WebFontLoader from 'webfontloader'
+// Globals
+import { IN_BROWSER, IS_SERVER } from '@/util/globals'
 
-// export function loadFonts () {
-//   WebFontLoader.load({
-//     google: {
-//       families: ['Roboto:100,300,400,500,700,900&display=swap'],
-//     },
-//     custom: {
-//       families: [
-//         'Material Design Icons',
-//         'Font Awesome 5',
-//       ],
-//       urls: [
-//         'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css',
-//         'https://use.fontawesome.com/releases/v5.0.8/css/all.css',
-//       ],
-//     },
-//   })
-// }
+export async function loadFonts () {
+  if (IS_SERVER || !IN_BROWSER) return
+
+  const WebFontLoader = await import('webfontloader')
+
+  WebFontLoader.load({
+    google: {
+      families: ['Roboto:100,300,400,500,700,900&display=swap'],
+    },
+    custom: {
+      families: [
+        'Material Design Icons',
+        'Font Awesome 5',
+      ],
+      urls: [
+        'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css',
+        'https://use.fontawesome.com/releases/v5.0.8/css/all.css',
+      ],
+    },
+  })
+}
